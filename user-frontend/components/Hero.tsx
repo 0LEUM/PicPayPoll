5906dee2d678a1bf6bd9d8e833d4675d1848879d
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2024-05-26 09:37:22 +0530

    Add files via upload

---
+++ user-frontend/components/Hero.tsx
@@ -0,0 +1,11 @@
+
+export const Hero = () => {
+    return <div className="text-black pt-10">
+        <div className="text-2xl flex justify-center">
+            Welcome to PicPayPoll
+        </div>
+        <div className="text-lg flex justify-center pt-8">
+            Your one stop destination to get opinion...
+        </div>
+    </div>
+}
---
+++ user-frontend/utils/index.ts
@@ -0,0 +1,4 @@
+
+export const BACKEND_URL = "http://localhost:3000";
+export const CLOUDFRONT_URL = "https://d33140gluhdd3j.cloudfront.net";
+export const RPC_URL = "https://solana-mainnet.g.alchemy.com/v2/Y3x_-dIBlj2-S13Gu9Rz3aoCpcHnmtSF"
