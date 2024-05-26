ed8933a7607e76048c283c5c6990b7ec9decfef5
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2024-05-26 09:41:12 +0530

    Add files via upload

---
+++ worker-frontend/app/(root)/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { RPC_URL } from "@/utils";
+import {
+    WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const network = WalletAdapterNetwork.Mainnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = RPC_URL;
+
+  const wallets = useMemo(
+      () => [],
+      [network]
+  );
+
+    return (
+    <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+                {children}
+            </WalletModalProvider>
+        </WalletProvider>
+    </ConnectionProvider>
+  );
+}
