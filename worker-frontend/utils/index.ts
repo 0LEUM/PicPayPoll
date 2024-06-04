6ea572d693c25c6d602cccb7698b6fdc15bc3b51
Ritam Chakraborty <104848386+0LEUM@users.noreply.github.com>
2024-06-04 09:28:52 +0530

    Update index.ts

--- worker-frontend/utils/index.ts
+++ worker-frontend/utils/index.ts
@@ -1,4 +1,4 @@
 
 export const BACKEND_URL = "http://localhost:3000";
-export const CLOUDFRONT_URL = "https://d2szwvl7yo497w.cloudfront.net";
-export const RPC_URL = "https://solana-mainnet.g.alchemy.com/v2/Y3x_-dIBlj2-S13Gu9Rz3aoCpcHnmtSF"
+export const CLOUDFRONT_URL = "AWS Cloudfront URL";
+export const RPC_URL = "Remote Procedure Calls URL"
